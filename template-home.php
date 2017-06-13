41c6ab79b50f1a518e67a13cc91ada6ec502e201
Craig <jimmy.zooler@gmail.com>
2017-06-13 18:56:29 -0400

    Added home page masonry styles and javascript.

--- template-home.php
+++ template-home.php
@@ -30,11 +30,47 @@ if( $images ): ?>
     
 </div><!-- Wrapper end -->
 
-<div class="wrapper">
+<div class="wrapper" id="home-page-masonry">
 
     <main class="site-main" id="main" role="main">
 
-            
+            <?php
+
+            //* The Query
+            $exec_query = new WP_Query( array (
+              'post_type' => 'gallery'
+            ) );
+
+            //* The Loop
+            if ( $exec_query->have_posts() ) { ?>
+
+                  <div id="grid" data-columns> <?php
+
+                while ( $exec_query->have_posts() ): $exec_query->the_post(); ?>
+
+                    <div>
+                        <a href="<?php the_permalink(); ?>">
+                          <img src="<?php 
+                
+                            $thumb_id = get_post_thumbnail_id();
+                            $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
+                            echo $thumb_url_array[0]; 
+                                    
+                                    ?>" />
+                        </a>
+                        <a href="<?php the_permalink(); ?>">
+                          <h2><?php the_title(); ?></h2>
+                        </a>
+                    </div> <?php
+
+                endwhile; ?>
+
+                  </div> <?php
+
+                //* Restore original Post Data
+                wp_reset_postdata();
+
+            } ?>
 
         </main><!-- #main -->
 
