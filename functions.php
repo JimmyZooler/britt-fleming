385a14239933da901f7c2fddd5eb6b9829a90e5e
Craig <jimmy.zooler@gmail.com>
2017-06-13 16:37:33 -0400

    took out version number from scripts path.

--- functions.php
+++ functions.php
@@ -18,5 +18,5 @@ function theme_enqueue_styles() {
 
     wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
-    wp_enqueue_script( 'custom-scripts', get_stylesheet_directory_uri() . '/src/js/scripts.js', array(), $the_theme->get( 'Version' ), true );
+    wp_enqueue_script( 'custom-scripts', get_stylesheet_directory_uri() . '/src/js/scripts.js', array(), true );
 }
