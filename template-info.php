39f7487764c0a5731c99ca10874d893c1df74b30
Craig <jimmy.zooler@gmail.com>
2017-06-16 12:34:48 -0400

    Added fields, markup, and styles to info page.

--- template-info.php
+++ template-info.php
@@ -10,15 +10,36 @@
 get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
+<main class="site-main" id="main" role="main">
+    <div class="wrapper" id="info-page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="<?php echo get_field('image'); ?>" alt="Brittany Fleming exploring the streets of California" />
+                </div>
+                <div class="col-md-6">
+                        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+                        the_content();
+                        endwhile; ?>
+                        <?php endif; ?>
+                </div>
+            </div>
+            <div class="row" id="contact">
 
-<div class="wrapper" id="info-page-content">
+                <h2><?php echo get_field('heading'); ?></h2>
 
-    <main class="site-main" id="main" role="main">
+                <div class="col-md-6 column-contact">
+                    <span class="contact-info" id="emailAddress">b_fleming@live.com</span>
+                </div>
+                <div class="col-md-6 column-contact">
+                    <span class="contact-info" id="phoneNumber">613-612-1463</span>
+                </div>
 
-            
+            </div>
+        </div>
+    </div>    
+</main><!-- #main -->
 
-    </main><!-- #main -->
 
-</div>
 
 <?php get_footer(); ?>
