903ffa8645060965f830d4fec37d48aa99b8f455
Craig <jimmy.zooler@gmail.com>
2017-06-16 11:32:56 -0400

    Made nav slightly transparent. Added info page template.

---
+++ template-info.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Info Page
+ *
+ * Template for displaying the info (contact) page.
+ *
+ * @package understrap
+ */
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="wrapper" id="info-page-content">
+
+    <main class="site-main" id="main" role="main">
+
+            
+
+    </main><!-- #main -->
+
+</div>
+
+<?php get_footer(); ?>
